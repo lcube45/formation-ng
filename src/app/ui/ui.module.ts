bf5424f7bd5879ad471c97322eaa166f4cd1a160
lcube <info@lcube.fr>
2019-10-14 16:59:15 +0200

    created 3 components in our ui module

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiComponent } from './containers/ui/ui.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
-  declarations: [UiComponent],
+  declarations: [UiComponent, HeaderComponent, FooterComponent, NavComponent],
   imports: [
     CommonModule
   ]
